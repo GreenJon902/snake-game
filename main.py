ee811bc7690c0b66a51c38194490cef0b3e62156
GreenJon902 <GreenJon902@gmail.com>
2021-06-13 11:18:33 +0100

    draw bg

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import sys
+
 import pygame
+from pygame import QUIT, KEYDOWN, K_ESCAPE
+
+from static_vars import *
 
 pygame.init()
+
+DISPLAY = pygame.display.set_mode(window_size, 0, 32)
+
+
+def draw():
+    DISPLAY.fill(background_color)
+
+    for sx in range(horizontal_square_amount):
+        x = sx * square_size + sx * square_gap + square_gap
+
+        for sy in range(vertical_square_amount):
+            y = sy * square_size + sy * square_gap + square_gap
+
+            pygame.draw.rect(DISPLAY, square_color, (x, y, square_size, square_size))
+
+    pygame.display.update()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+        draw()
---
+++ static_vars.py
@@ -0,0 +1,11 @@
+# Colors
+background_color = (50, 50, 50)
+square_color = (70, 70, 70)
+
+# Sizes
+square_size = 45
+square_gap = 5
+horizontal_square_amount = 10
+vertical_square_amount = 10
+window_size = ((square_size + square_gap) * horizontal_square_amount + square_gap,
+               (square_size + square_gap) * vertical_square_amount + square_gap)
