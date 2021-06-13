3253c43fc3486c79441a0c3989454e7fa31e1c39
GreenJon902 <GreenJon902@gmail.com>
2021-06-13 12:49:25 +0100

    text

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ pygame.display.set_caption("Snake - GreenJon902")
 snake_poses = initial_snake_poses
 snake_direction = initial_snake_direction
 food_pos = "no"
+score = 0
 
 
 def draw():
@@ -24,20 +25,33 @@ def draw():
         x = sx * square_size + sx * square_gap + square_gap
 
         for sy in range(vertical_square_amount):
-            y = sy * square_size + sy * square_gap + square_gap
+            y = sy * square_size + sy * square_gap + square_gap + top_height
 
             pygame.draw.rect(DISPLAY, square_color, (x, y, square_size, square_size))
 
 
     pygame.draw.rect(DISPLAY, food_color, (food_pos[0] * square_size + food_pos[0] * square_gap + square_gap,
-                                            food_pos[1] * square_size + food_pos[1] * square_gap + square_gap,
+                                            food_pos[1] * square_size + food_pos[1] * square_gap + square_gap +
+                                           top_height,
                                             square_size, square_size))
 
 
     for pos in snake_poses:
         pygame.draw.rect(DISPLAY, snake_color, (pos[0] * square_size + pos[0] * square_gap + square_gap,
-                                                pos[1] * square_size + pos[1] * square_gap + square_gap,
+                                                pos[1] * square_size + pos[1] * square_gap + square_gap + top_height,
                                                 square_size, square_size))
+
+
+    font = pygame.font.Font(font_path, font_size)
+
+    text = font.render("Snake - GreenJon902", False, text_color)
+    DISPLAY.blit(text, (text_padding, top_height / 2 - text.get_height() / 2))
+
+    score_text = font.render("Score - " + str(score), False, text_color)
+    DISPLAY.blit(score_text, (window_size[0] - score_text.get_width() - text_padding,
+                              top_height / 2 - score_text.get_height() / 2))
+
+
     pygame.display.update()
 
 
@@ -70,6 +84,7 @@ def move_snake():
 
 def check_food():
     global food_pos
+    global score
 
     if food_pos == "no":
         food_pos = [random.randint(0, horizontal_square_amount - 1), random.randint(0, vertical_square_amount - 1)]
@@ -79,8 +94,10 @@ def check_food():
     if snake_poses[len(snake_poses) - 1] == food_pos:
         food_pos = [random.randint(0, horizontal_square_amount - 1), random.randint(0, vertical_square_amount - 1)]
         snake_poses.append(snake_poses[len(snake_poses) - 1])
+        score += 1
 
         print("Spawned food at", food_pos)
+        print("Score is now", score)
 
 
 update_timer = 0.0
